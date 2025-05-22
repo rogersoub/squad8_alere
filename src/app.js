47ae1a0d760243950ce732201c0245e2ba550350
roger <rogersoubrand1a@gmail.com>
2025-05-22 11:48:46 -0300

    deploy

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 // CORS configurado
-const cors_config = { origin: '*' };
+const cors_config = { origin: 'http://localhost:3000' };
 
 // Middlewares
 app.use(logger);
